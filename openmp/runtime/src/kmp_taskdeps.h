jpinot/llvm-project

commit 8a2da514c8c318fdbaa21c9962dc147ffc06d482

[FIX] fixing a rare taskgraph dependency tracking issue. This only happens when a task is complete before its dependencies are tracked for later tasks.

openmp/runtime/src/kmp_taskdeps.h
@@ -169,8 +169,9 @@ static inline void __kmp_release_deps(kmp_int32 gtid, kmp_taskdata_t *task) {
                 gtid, task));
 
   KMP_ACQUIRE_DEPNODE(gtid, node);
-  node->dn.task =
-      NULL; // mark this task as finished, so no new dependencies are generated
+  if (!(task->tdg->tdgStatus==TDG_RECORDING))
+    node->dn.task =
+        NULL; // mark this task as finished, so no new dependencies are generated
   KMP_RELEASE_DEPNODE(gtid, node);
 
   kmp_depnode_list_t *next;

openmp/runtime/src/kmp.h
@@ -2493,7 +2493,8 @@ typedef struct kmp_tasking_flags { /* Total struct must be exactly 32 bits */
   unsigned complete : 1; /* 1==complete, 0==not complete   */
   unsigned freed : 1; /* 1==freed, 0==allocated        */
   unsigned native : 1; /* 1==gcc-compiled task, 0==intel */
-  unsigned reserved31 : 7; /* reserved for library use */
+  unsigned onced : 1; /* 1==ran once already, 0==never ran, record & replay purposes */
+  unsigned reserved31 : 6; /* reserved for library use */
 
 } kmp_tasking_flags_t;
 

openmp/runtime/src/kmp_taskdeps.cpp
@@ -407,6 +407,16 @@ static inline kmp_int32 __kmp_depnode_link_successor(kmp_int32 gtid,
                     "%p\n",
                     gtid, KMP_TASK_TO_TASKDATA(sink->dn.task),
                     KMP_TASK_TO_TASKDATA(task)));
+#if LIBOMP_TASKGRAPH
+      if (tdgStatus == TDG_RECORDING) {
+        kmp_taskdata_t *td = KMP_TASK_TO_TASKDATA(sink->dn.task);
+        if (td->is_taskgraph && td->td_flags.onced)
+          // decrement npredecessors only if sink->dn.task belongs to a taskgraph and
+          // 1. the task is reset to its initial state (by kmp_free_task) or
+          // 2. the task is complete but not yet reset
+          npredecessors--;
+      }
+#endif
       npredecessors++;
     }
     KMP_RELEASE_DEPNODE(gtid, sink);

openmp/runtime/src/kmp_tasking.cpp
@@ -1267,6 +1267,7 @@ static void __kmp_task_finish(kmp_int32 gtid, kmp_task_t *task,
 
   if (completed) {
     taskdata->td_flags.complete = 1; // mark the task as completed
+    taskdata->td_flags.onced = 1; // mark the task as ran once already
 
 #if OMPT_SUPPORT
     // This is not a detached task, we are done here
@@ -1461,6 +1462,7 @@ void __kmp_init_implicit_task(ident_t *loc_ref, kmp_info_t *this_thr,
   task->td_flags.executing = 1;
   task->td_flags.complete = 0;
   task->td_flags.freed = 0;
+  task->td_flags.onced = 0;
 
   task->td_depnode = NULL;
   task->td_last_tied = task;
@@ -1500,6 +1502,7 @@ void __kmp_finish_implicit_task(kmp_info_t *thread) {
   if (task->td_dephash) {
     int children;
     task->td_flags.complete = 1;
+    task->td_flags.onced = 1;
     children = KMP_ATOMIC_LD_ACQ(&task->td_incomplete_child_tasks);
     kmp_tasking_flags_t flags_old = task->td_flags;
     if (children == 0 && flags_old.complete == 1) {
@@ -1744,6 +1747,7 @@ kmp_task_t *__kmp_task_alloc(ident_t *loc_ref, kmp_int32 gtid,
   taskdata->td_flags.executing = 0;
   taskdata->td_flags.complete = 0;
   taskdata->td_flags.freed = 0;
+  taskdata->td_flags.onced = 0;
 
   KMP_ATOMIC_ST_RLX(&taskdata->td_incomplete_child_tasks, 0);
   // start at one because counts current task and children
@@ -5288,7 +5292,7 @@ static void __kmp_first_top_half_finish_proxy(kmp_taskdata_t *taskdata) {
   KMP_DEBUG_ASSERT(taskdata->td_flags.freed == 0);
 
   taskdata->td_flags.complete = 1; // mark the task as completed
-
+  taskdata->td_flags.onced = 1; // mark the task as ran once
   if (taskdata->td_taskgroup) {
     KMP_ATOMIC_DEC(&taskdata->td_taskgroup->count);
   }

commit 8060c0fea1998c064ac6b0ab17cad7fc81b9e0cc

[FIX] Seg fault bug caused by 8a2da514, where we access the taskdata->tdg while it is null

openmp/runtime/src/kmp_taskdeps.h
@@ -170,7 +170,8 @@ static inline void __kmp_release_deps(kmp_int32 gtid, kmp_taskdata_t *task) {
 
   KMP_ACQUIRE_DEPNODE(gtid, node);
 #ifdef LIBOMP_TASKGRAPH
-  if (!(task->tdg->tdgStatus==TDG_RECORDING))
+  if (!task->is_taskgraph ||
+      (task->is_taskgraph && !(task->tdg->tdgStatus==TDG_RECORDING)))
 #endif
     node->dn.task =
         NULL; // mark this task as finished, so no new dependencies are generated