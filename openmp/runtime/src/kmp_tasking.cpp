413cb12b35307ec66e8fa543d594008d10ffea12
jpinot <josep.pinot@bsc.es>
2025-03-31 14:06:31 +0200

    [xxx][wip][openmp] Add comments for double task free

--- openmp/runtime/src/kmp_tasking.cpp
+++ openmp/runtime/src/kmp_tasking.cpp
@@ -2036,6 +2036,7 @@ kmp_int32 __kmp_omp_task(kmp_int32 gtid, kmp_task_t *new_task,
         KMP_MEMCPY(new_record, old_record, old_size * sizeof(kmp_node_info_t));
         tdg->record_map = new_record;
 
+        // XXX: double free
         __kmp_free(old_record);
 
         for (kmp_int i = old_size; i < new_size; i++) {
@@ -5608,6 +5609,7 @@ static inline void __kmp_start_record(kmp_int32 gtid,
     kmp_int32 *successorsList =
         (kmp_int32 *)__kmp_allocate(__kmp_successors_size * sizeof(kmp_int32));
     this_record_map[i].task = nullptr;
+    this_record_map[i].parent_task = nullptr;
     this_record_map[i].successors = successorsList;
     this_record_map[i].nsuccessors = 0;
     this_record_map[i].npredecessors = 0;
