b8f2188ce891a10498e018f451c2d75f190ff5ba
jpinot <josep.pinot@bsc.es>
2025-04-02 08:55:33 +0200

    Revert "[xxx][wip][openmp] Moved double task free to the end of scope"
    This reverts commit e976d390b4ed8028e038487b2276f4867705e906.

--- openmp/runtime/src/kmp_tasking.cpp
+++ openmp/runtime/src/kmp_tasking.cpp
@@ -2034,11 +2034,8 @@ kmp_int32 __kmp_omp_task(kmp_int32 gtid, kmp_task_t *new_task,
         KMP_MEMCPY(new_record, old_record, old_size * sizeof(kmp_node_info_t));
         tdg->record_map = new_record;
 
-        if (!old_record) {
-          printf("--->TASKING: WHAT DA FUUU\n");
-        } else {
-          /* printf("--->TASKING: todo bien\n"); */
-        }
+        // XXX: double free
+        /* __kmp_free(old_record); */
 
         for (kmp_int i = old_size; i < new_size; i++) {
           kmp_int32 *successorsList = (kmp_int32 *)__kmp_allocate(
@@ -2053,9 +2050,6 @@ kmp_int32 __kmp_omp_task(kmp_int32 gtid, kmp_task_t *new_task,
         // update the size at the end, so that we avoid other
         // threads use old_record while map_size is already updated
         tdg->map_size = new_size;
-
-        // XXX: double free
-        __kmp_free(old_record);
       }
       __kmp_release_bootstrap_lock(&tdg->graph_lock);
     }
