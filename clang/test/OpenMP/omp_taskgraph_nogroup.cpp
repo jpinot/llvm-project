18b1deced843972884f2013a08b41a98644f6c64
jpinot <jsp.pinot@gmail.com>
2025-09-29 12:58:22 +0200

    [clang] Add clause nowait/nogroup in taskgraph

---
+++ clang/test/OpenMP/omp_taskgraph_nogroup.cpp
@@ -0,0 +1,23 @@
+// RUN: %clang_cc1 -fopenmp -x c++ -triple x86_64-unknown-unknown -emit-llvm -fexceptions -fcxx-exceptions -o - %s | FileCheck %s
+// RUN: %clang_cc1 -fopenmp-simd -x c++ -triple x86_64-unknown-unknown -emit-llvm -fexceptions -fcxx-exceptions -o - %s | FileCheck --check-prefix SIMD-ONLY0 %s
+// SIMD-ONLY0-NOT: {{__kmpc|__tgt}}
+
+int main() {
+  int foo = 0;
+
+  // CHECK: call void @__kmpc_taskgraph(ptr @{{.*}}, i32 {{.*}}, i32 1, i32 {{.*}}, i32 {{.*}}, ptr @{{.*}}.omp_outlined{{.*}}, ptr {{.*}})
+  #pragma omp taskgraph nogroup
+  {
+    #pragma omp task
+    foo++;
+  }
+
+  // CHECK: call void @__kmpc_taskgraph(ptr @{{.*}}, i32 {{.*}}, i32 0, i32 {{.*}}, i32 {{.*}}, ptr @{{.*}}.omp_outlined{{.*}}, ptr {{.*}})
+  #pragma omp taskgraph
+  {
+    #pragma omp task
+    foo++;
+  }
+
+  return 0;
+}
